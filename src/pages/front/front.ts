7f91b3228637cc9570e8bdd6e7a69a974745ad71
anniluo <anniluo@metropolia.fi>
2018-02-07 14:00:24 +0200

    readme file

--- src/pages/front/front.ts
+++ src/pages/front/front.ts
@@ -15,7 +15,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class FrontPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams ) {
   }
 
   ionViewDidLoad() {
